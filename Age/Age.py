f1310680b4e74d610e494fade5e972f8494ae465
ShellofaGhost <89920392+scoffey1@users.noreply.github.com>
2025-02-06 17:48:15 -0500

    Update Age.py
    Added comments explaining logic

--- Age/Age.py
+++ Age/Age.py
@@ -9,10 +9,10 @@ class Person:
     @classmethod
     def from_birth_year(cls, name, birth_year):
         """Return an instance of Person with their age calculated"""
-        current_year = datetime.now().year
+        current_year = datetime.now().year 
         #current_year = 2025
         return cls(name, current_year - birth_year)
     
-john = Person("John", 24)
-new_person = Person.from_birth_year("Mary", 1984)
+john = Person("John", 24) # --> init
+new_person = Person.from_birth_year("Mary", 1984) # --> class methods --> init
 print(vars(new_person))

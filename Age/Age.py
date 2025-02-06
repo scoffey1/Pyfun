b835a9faa8a5cfcdc45528dd640b7cf72e4a40a5
ShellofaGhost <89920392+scoffey1@users.noreply.github.com>
2025-02-06 17:41:22 -0500

    Add files via upload
    Used class methods and returned age dynamically.

---
+++ Age/Age.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+class Person:
+   
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    @classmethod
+    def from_birth_year(cls, name, birth_year):
+        """Return an instance of Person with their age calculated"""
+        current_year = datetime.now().year
+        #current_year = 2025
+        return cls(name, current_year - birth_year)
+    
+john = Person("John", 24)
+new_person = Person.from_birth_year("Mary", 1984)
+print(vars(new_person))
